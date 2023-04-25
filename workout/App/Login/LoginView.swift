b98bdc06fecc115d413a228af4a13edf474a0f46
Guilherme dos Santos <guilherme@guisantos.com.br>
2023-04-25 18:58:14 -0300

    Login com firebase

--- workout/App/Login/LoginView.swift
+++ workout/App/Login/LoginView.swift
@@ -13,9 +13,21 @@ class LoginView: BaseUIView {
         let button = UIButton()
         button.translatesAutoresizingMaskIntoConstraints = false
         button.setTitle("Login Com Apple", for: .normal)
-        button.backgroundColor = UIColor.red
         button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .black
         return button
+    }()
+    
+    lazy var image: UIImageView = {
+        let view = UIImageView(image: .init(named: "HomeIcon"))
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    lazy var bottomContainer: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
     }()
     
     init() {
@@ -26,19 +38,28 @@ class LoginView: BaseUIView {
 
 extension LoginView: ViewCode {
     func buildViewHierrachy() {
-        self.addSubview(loginButton)
+        self.addSubview(image)
+        self.addSubview(bottomContainer)
+        self.bottomContainer.addSubview(loginButton)
     }
     
     func setupConstraints() {
         NSLayoutConstraint.activate([
-            self.loginButton.widthAnchor.constraint(equalToConstant: 200),
-            self.loginButton.heightAnchor.constraint(equalToConstant: 45),
-            self.loginButton.centerXAnchor.constraint(equalTo: self.centerXAnchor),
-            self.loginButton.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            self.image.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            self.image.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            self.image.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 41),
+            self.image.heightAnchor.constraint(equalToConstant: 300),
+            self.bottomContainer.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            self.bottomContainer.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            self.bottomContainer.topAnchor.constraint(equalTo: self.image.bottomAnchor, constant: 60),
+            self.bottomContainer.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor, constant: -20),
+            self.loginButton.bottomAnchor.constraint(equalTo: self.bottomContainer.bottomAnchor),
+            self.loginButton.trailingAnchor.constraint(equalTo: self.bottomContainer.trailingAnchor),
+            self.loginButton.leadingAnchor.constraint(equalTo: self.bottomContainer.leadingAnchor),
         ])
     }
     
     func setupAdditionalConfiguration() {
-        
+        self.backgroundColor = .white
     }
 }
